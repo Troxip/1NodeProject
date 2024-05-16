b526bd38987f647ff16227988cf177bf9e15a81d
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-05-16 18:14:19 +0200

    Learn middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,12 +4,11 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 router.param('id', tourController.checkID);
-router.param('id', tourController.checkBody);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
