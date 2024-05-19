b571d55a129c413d816b3ea0c278238ea8bc7b4a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-05-19 21:13:04 +0200

    mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+mongoose
+  // local database version
+  // .connect(process.env.DATABASE_LOCAL, {
+  // hosted database version
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
